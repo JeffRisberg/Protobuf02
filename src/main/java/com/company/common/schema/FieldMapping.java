a749de2dbe45a71a76a2e06fd1924b382d6aeee9
JeffRisberg <jrisberg@aol.com>
2018-08-20 23:31:14 -0700

    added fieldmapping data structure

---
+++ src/main/java/com/company/common/schema/FieldMapping.java
@@ -0,0 +1,60 @@
+package com.company.common.schema;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "field_mappings")
+@Data
+public class FieldMapping {
+  @Id
+  @Column(name = "id")
+  protected Long id;
+
+  // Scoping information
+
+  @Column(name = "tenant_id")
+  @NotNull
+  protected String tenantId;
+
+  @Column(name = "entity_name")
+  @NotNull
+  protected String entityName;
+
+  @Column(name = "version")
+  protected Integer version;
+
+  @Column(name = "seq_num")
+  @NotNull
+  protected int seqNum;
+
+  @Column(name = "mandatory")
+  protected boolean mandatory;
+
+  // Aisera-side information
+
+  @Column(name = "field_name")
+  protected String fieldName;
+
+  @Column(name = "field_type")
+  protected FieldType fieldType;
+
+  @Column(name = "field_enum_values")
+  protected String fieldNumValues;
+
+  // External-side information
+
+  @Column(name = "external_field_name")
+  protected String externalFieldName;
+
+  @Column(name = "external_field_type")
+  protected FieldType externalFieldType;
+
+  @Column(name = "external_field_enum_values")
+  protected String externalFieldNumValues;
+}
---
+++ src/main/java/com/company/common/schema/FieldType.java
@@ -0,0 +1,10 @@
+package com.company.common.schema;
+
+public enum FieldType {
+  String,
+  Integer,
+  Boolean,
+  Date,
+  Enum,
+  Content
+}
