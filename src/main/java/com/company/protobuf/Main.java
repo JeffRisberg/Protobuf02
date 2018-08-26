a3fbf57bbebd444b0668c9758e9140252e53495c
JeffRisberg <jrisberg@aol.com>
2018-08-25 18:02:04 -0700

    added custom field example

--- src/main/java/com/company/protobuf/Main.java
+++ src/main/java/com/company/protobuf/Main.java
@@ -1,5 +1,6 @@
 package com.company.protobuf;
 
+import com.company.common.CustomField;
 import com.company.common.TicketContent;
 import com.company.common.TicketContentOrBuilder;
 
@@ -8,17 +9,44 @@ import java.util.UUID;
 
 public class Main {
 
-  public static void main(String[] args) {
+  public Main() {
     UUID sysId = java.util.UUID.randomUUID();
     String name = "Technical Catalog";
 
     TicketContent.Builder builder = TicketContent.newBuilder();
-    builder.setId("ddd");
-    builder.setDisplayId("ddd");
+    builder.setId(sysId.toString());
+    builder.setDisplayId(name);
 
     TicketContent ticket = builder.build();
 
     // serialize
     System.out.println("serializing");
+    System.out.println(ticket);
+  }
+
+  public void customTest() {
+    UUID sysId = java.util.UUID.randomUUID();
+    String name = "Technical Catalog";
+
+    TicketContent.Builder ticketBuilder = TicketContent.newBuilder();
+    ticketBuilder.setId(sysId.toString());
+    ticketBuilder.setDisplayId(name);
+
+    CustomField.Builder cfBuilder = CustomField.newBuilder();
+    cfBuilder.setName("category");
+    cfBuilder.setValue("alpha");
+    ticketBuilder.addCustomFields(cfBuilder.build());
+
+    TicketContent ticket = ticketBuilder.build();
+
+    // serialize
+    System.out.println("serializing");
+    System.out.println(ticket);
+  }
+
+  public static void main(String[] args) {
+    Main main = new Main();
+
+    main.customTest();
   }
 }
